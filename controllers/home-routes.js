28e192ffbd49839c1f29947952b5fe38f94b2f7e
Anders Newgard <anne9707@colorado.edu>
2023-10-29 16:50:21 -0400

    set up index.js controller file. began writing logic for GET requests for home routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+// GET all posts for homepage. Read operation. 
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [User],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('all-posts', { posts });
+        } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET a single post. Read operation.
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                User,
+                {
+                    model: Comment,
+                    include: [User],
+                },
+            ],
+        });
+
+        if (postData) {
+            const post = postData.get({ plain: true });
+
+            res.render('single-post', { post });
+        } else {
+            res.status(404).end();
+        }
+        } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
