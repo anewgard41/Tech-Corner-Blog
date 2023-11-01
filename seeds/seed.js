bc3fca3556b3f1fa6fdbaedc7345d5dfefce900b
Anders Newgard <anne9707@colorado.edu>
2023-10-31 23:10:05 -0400

    created artificial data and seed.js file.

---
+++ seeds/seed.js
@@ -0,0 +1,29 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    
+  const users = await User.bulkCreate(userData, {
+    // This is to make sure the passwords are hashed.
+    individualHooks: true,
+    returning: true,
+  });
+
+  // Loops through the postData array and creates a new post for each entry.
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      // Randomly assigns a user_id from the users array to each post.
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
