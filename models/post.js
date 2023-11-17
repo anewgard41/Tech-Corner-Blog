913e9ea8780c6bfc62b3aa64366b6b6ffced40ee
Anders Newgard <anne9707@colorado.edu>
2023-11-17 17:05:57 -0500

    once again messing with casing so that heroku is pleased and that usernames show up for posts. I think we've done it.

--- models/post.js
+++ models/post.js
@@ -29,7 +29,7 @@ Post.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             references: {
-              model: 'user',
+              model: 'User',
               key: 'id',
             },
         },
